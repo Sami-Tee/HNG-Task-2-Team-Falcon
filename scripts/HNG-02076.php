03ace4e9617f15507498547836a5d39242630393
Sami-Tee <Sami-Tee@users.noreply.github.com>
2020-06-03 10:22:56 +0100

    Update HNG-02076.php

--- scripts/HNG-02076.php
+++ scripts/HNG-02076.php
@@ -1,7 +1,7 @@
 + <?php
 + echo "Hello World,
-+ This is Effa Samuel Oku
-+ With HNG17 ID[02076]
++ This is Effa Samuel Oku,
++ With HNG17 ID[02076],
 + using [PHP],
   with email address [ssmmyeffa@gmail.com]" ;
 ?>
